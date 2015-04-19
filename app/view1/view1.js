cb8234c5a6decc29e56503dc031aeef65ea56dfe
Nicholas Wong <nickwph@gmail.com>
2015-04-19 01:10:03 -0400

    Updated view1 html and js to show the panel.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,7 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+// create our angular app and inject ui.bootstrap
+angular.module('myApp.view1', ['ngRoute', 'ui.bootstrap'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -11,4 +12,20 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', [function() {
 
-}]);
+}])
+
+.controller('mainController', function($scope) {
+
+  // BUTTONS ======================
+
+  // define some random object
+  $scope.bigData = {};
+
+  $scope.bigData.breakfast = false;
+  $scope.bigData.lunch = false;
+  $scope.bigData.dinner = false;
+
+  // COLLAPSE =====================
+  $scope.isCollapsed = false;
+
+});
